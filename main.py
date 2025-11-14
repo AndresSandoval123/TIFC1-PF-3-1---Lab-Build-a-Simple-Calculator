a90f4dd440bd1c033a243aa7f4b85a458b29f1e5
Andres Sandoval <andresfelipe3088@gmail.com>
2025-11-13 20:05:13 -0500

    prueba

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
 num1 = int(input("ingrese el primer numero "))
 num2 = int(input("ingrese el segundo numero "))
 
-suma = (num1 + num2)
-
-print("La suma de", num1 , "y", num2, "es: ", suma)
-
-# Gracias por su tiempo en la nivelacion  
-# Andres Sandoval
+suma 
