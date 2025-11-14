1a5c78af5dfbfe9b702840708c1c2e1f8325a974
Andres Felipe Sandoval Garcia <167252430+AndresSandoval123@users.noreply.github.com>
2025-11-13 19:51:19 -0500

    prueba de actualización

--- main.py
+++ main.py
@@ -5,4 +5,7 @@ suma = (num1 + num2)
 
 print("La suma de", num1 , "y", num2, "es: ", suma)
 
-# Gracias por su tiempo en la nivelacion
+# Gracias por su tiempo en la nivelacion  
+
+
+#jajajajaajajajajaja Hola a todos 
